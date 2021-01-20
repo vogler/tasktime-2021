6d899af21d1b9bdb3108aced87b33fa5c8eb6b5b
Ralf Vogler <ralf.vogler@gmail.com>
2021-01-20 16:53:58 +0100

    public -> frontend-static

--- snowpack.config.cjs
+++ snowpack.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
+    'frontend-static': {url: '/', static: true},
     frontend: {url: '/dist'},
     model: {url: '/dist'},
   },
