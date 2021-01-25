0fe1a4324c7ffcac2ad41f969ce982bbc3e2e6ea
Ralf Vogler <ralf.vogler@gmail.com>
2021-01-25 11:18:21 +0100

    example sqlite.db with prisma

--- backend/server.ts
+++ backend/server.ts
@@ -7,6 +7,17 @@ const port = 8080;
 // app.use(compress());
 // app.use(express.static('frontend-static')); // snowpack modifes index.html for HMR
 
+// access sqlite database with prisma:
+// import { PrismaClient } from '@prisma/client'; // SyntaxError: Named export 'PrismaClient' not found. The requested module '@prisma/client' is a CommonJS module, which may not support all module.exports as named exports.
+// CommonJS modules can always be imported via the default export, for example...
+import prisma from '@prisma/client';
+const db = new prisma.PrismaClient();
+
+app.get("/posts", async (req: Request, res: Response) => {
+  const posts = await db.post.findMany();
+  res.json(posts);
+});
+
 const todos = [{
   date: Date.now(),
   text: 'Todo 1',
@@ -28,6 +39,7 @@ app.listen(port, () => {
 });
 
 
+// snowpack build on demand and SSR:
 // https://www.snowpack.dev/guides/server-side-render#option-2%3A-on-demand-serving-(middleware)
 import { startServer, loadConfiguration } from 'snowpack';
 // TODO remove open: 'none' once we can set port to open: https://github.com/snowpackjs/snowpack/discussions/2415
