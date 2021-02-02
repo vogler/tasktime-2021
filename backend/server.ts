f885c27dc8482906be681d01fca7b50b7e6f1637
Ralf Vogler <ralf.vogler@gmail.com>
2021-02-02 03:00:18 +0100

    heroku: use $PORT

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 // app.use(cookieParser());
