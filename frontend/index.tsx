171201736a22d544144fd7035e7a7cd9b2eb1c9d
Ralf Vogler <ralf.vogler@gmail.com>
2021-03-09 10:35:38 +0100

    npm i focus-visible: disable the accessibility outline for non-keyboard uses

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -7,6 +7,7 @@ import App from './App';
 window.React = React; // so that we do not need to import React in every .tsx just to be able to use JSX
 
 // const theme = extendTheme({components: {Button: {defaultProps:{ size: "sm" }}}}); // use in ChakraProvider
+import "focus-visible/dist/focus-visible"; // https://github.com/chakra-ui/chakra-ui/issues/3449
 
 ReactDOM.render(
   <React.StrictMode>
