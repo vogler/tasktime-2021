800a26a57097b9bda9f5a361b9374081fdfa1407
Ralf Vogler <ralf.vogler@gmail.com>
2021-03-10 12:48:10 +0100

    replace focus-visible polyfill by css

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -7,7 +7,11 @@ import App from './App';
 window.React = React; // so that we do not need to import React in every .tsx just to be able to use JSX
 
 // const theme = extendTheme({components: {Button: {defaultProps:{ size: "sm" }}}}); // use in ChakraProvider
-import "focus-visible/dist/focus-visible"; // https://github.com/chakra-ui/chakra-ui/issues/3449
+
+// Disable the accessibility outline for non-keyboard uses
+// https://github.com/chakra-ui/chakra-ui/issues/3449
+// import "focus-visible/dist/focus-visible"; // polyfill, but was slow in waterfall graph
+import './index.css'; // just adjust .js-focus-visible there
 
 ReactDOM.render(
   <React.StrictMode>
