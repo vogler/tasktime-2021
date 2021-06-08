971d25722ccd83f56e5678976b8f9e3b7479ed5a
Ralf Vogler <ralf.vogler@gmail.com>
2021-06-08 13:05:53 +0200

    clean: false does not help, set out to '/tmp/snowpack'

--- snowpack.config.cjs
+++ snowpack.config.cjs
@@ -16,8 +16,10 @@ module.exports = {
     target: 'es2018', // es2020 is the highest, but also replaces const with var :(
   },
   buildOptions: {
+    // heroku fails removing old build files: 'rimrafSafe(): /tmp/build_3e0293ce/node_modules/@emotion/memoize/dist/emotion-memoize.browser.cjs.js outside of buildOptions.out /tmp/build_3e0293ce/build'
     // https://www.snowpack.dev/reference/configuration#buildoptions.clean
-    clean: false, // heroku fails removing old build files: 'rimrafSafe(): /tmp/build_3e0293ce/node_modules/@emotion/memoize/dist/emotion-memoize.browser.cjs.js outside of buildOptions.out /tmp/build_3e0293ce/build'
+    // clean: false, // does not help
+    out: '/tmp/snowpack',
   },
   packageOptions: {
     // source: 'remote', // npm deps from CDN instead of from node_modules
