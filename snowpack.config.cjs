48578855a8d63cba00726634ec8fa129dc3bfeda
Ralf Vogler <ralf.vogler@gmail.com>
2021-06-08 13:35:40 +0200

    snowpack: optimize.bundle: false since it now fails since node 15.14.0 (worked with 15.0.1)

--- snowpack.config.cjs
+++ snowpack.config.cjs
@@ -11,15 +11,12 @@ module.exports = {
     // '@snowpack/plugin-webpack', // bundles and minifies js -> hard to replace initialTodos. TODO put in index.html?
   ],
   optimize: { // uses esbuild, https://www.snowpack.dev/guides/optimize-and-bundle
-    bundle: true,
+    bundle: false, // deactivated for now, see https://github.com/snowpackjs/snowpack/issues/3403
+    // since node 15.14.0 (no problem with 15.0.1) fails removing old build files: 'rimrafSafe(): /tmp/build_3e0293ce/node_modules/@emotion/memoize/dist/emotion-memoize.browser.cjs.js outside of buildOptions.out /tmp/build_3e0293ce/build'
+    // tried buildOptions.clean: false, and buildOptions.out: '/tmp/snowpack' which did not help (server.ts reads from ./build; fails on macOS because it changes to /private/tmp)
+
     // minify: true, // we want to replace variables on the server
     target: 'es2018', // es2020 is the highest, but also replaces const with var :(
-  },
-  buildOptions: {
-    // heroku fails removing old build files: 'rimrafSafe(): /tmp/build_3e0293ce/node_modules/@emotion/memoize/dist/emotion-memoize.browser.cjs.js outside of buildOptions.out /tmp/build_3e0293ce/build'
-    // https://www.snowpack.dev/reference/configuration#buildoptions.clean
-    // clean: false, // does not help
-    out: './build', // default 'build'
   },
   packageOptions: {
     // source: 'remote', // npm deps from CDN instead of from node_modules
